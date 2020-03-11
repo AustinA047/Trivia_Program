d8688fb5da04148b24adacaaab6ee0866e672dd8
Austin Andrews <aandrews@debian07.CIS.COM>
2020-03-11 12:09:13 -0500

    YEYE

--- trivia_gui.py
+++ trivia_gui.py
@@ -33,22 +33,137 @@ class MainMenu(Screen):
         Screen.__init__(self)
         self.lbl_title = tk.Label(self, text = "Trivia Menu", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0)
-        self.btn_add = tk.Button(self, text = "History", font = BUTTON_FONT)
+        self.btn_add = tk.Button(self, text = "History", font = BUTTON_FONT, command = self.history)
         self.btn_add.grid(row = 1, column = 0)
-        self.btn_edit = tk.Button(self, text = "Geography", font = BUTTON_FONT)
+        self.btn_edit = tk.Button(self, text = "Geography", font = BUTTON_FONT, command = self.geography)
         self.btn_edit.grid(row = 2, column = 0)
-        self.btn_search = tk.Button(self, text = "Games", font = BUTTON_FONT)
+        self.btn_search = tk.Button(self, text = "Games", font = BUTTON_FONT, command = self.music)
         self.btn_search.grid(row = 3, column = 0)
-        self.btn_remove = tk.Button(self, text = "Music", font = BUTTON_FONT)
+        self.btn_remove = tk.Button(self, text = "Music", font = BUTTON_FONT, command = self.games)
         self.btn_remove.grid(row = 4, column = 0)
-        self.btn_save = tk.Button(self, text = "Random", font = BUTTON_FONT)
+        self.btn_save = tk.Button(self, text = "Random", font = BUTTON_FONT, command = self.random)
         self.btn_save.grid(row = 5, column = 0)
         
         self.grid_columnconfigure(0, weight = 1)
         self.grid_columnconfigure(1, weight = 1)  
         self.grid_columnconfigure(2, weight = 1)
         
+    def history(self):
+        Screen.current = 1
+        Screen.switch_frame()
+        
+    def geography(self):
+        Screen.current = 2
+        Screen.switch_frame()
+        
+    def music(self):
+        Screen.current = 3
+        Screen.switch_frame()  
+        
+    def games(self):
+        Screen.current = 4
+        Screen.switch_frame()
+        
+    def random(self):
+        Screen.current = 5
+        Screen.switch_frame()      
+        
 class History(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+        self.lbl_history = tk.Label(self, text ="History Quiz", font = TITLE_FONT)
+        self.lbl_history.grid(row = 0, column = 0, sticky = "news")
+        self.btn_start = tk.Button(self, text = "Start!", font = BUTTON_FONT)
+        self.btn_start.grid(row = 1, column = 0, sticky = "news")
+        self.btn_back = tk.Button(self, text = "Back", font = BUTTON_FONT, command = self.main)
+        self.btn_back.grid(row = 2, column = 0, sticky = "news")
+        
+        
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_columnconfigure(2, weight = 1)
+        
+    def main(self):
+        Screen.current = 0
+        Screen.switch_frame()      
+        
+class Geography(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+        self.lbl_history = tk.Label(self, text ="Geography Quiz", font = TITLE_FONT)
+        self.lbl_history.grid(row = 0, column = 0, sticky = "news")
+        self.btn_start = tk.Button(self, text = "Start!", font = BUTTON_FONT)
+        self.btn_start.grid(row = 1, column = 0, sticky = "news")
+        self.btn_back = tk.Button(self, text = "Back", font = BUTTON_FONT, command = self.main)
+        self.btn_back.grid(row = 2, column = 0, sticky = "news")
+        
+        
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_columnconfigure(2, weight = 1)
+        
+    def main(self):
+        Screen.current = 0
+        Screen.switch_frame()      
+        
+class Music(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+        self.lbl_history = tk.Label(self, text ="Music Quiz", font = TITLE_FONT)
+        self.lbl_history.grid(row = 0, column = 0, sticky = "news")
+        self.btn_start = tk.Button(self, text = "Start!", font = BUTTON_FONT)
+        self.btn_start.grid(row = 1, column = 0, sticky = "news")
+        self.btn_back = tk.Button(self, text = "Back", font = BUTTON_FONT, command = self.main)
+        self.btn_back.grid(row = 2, column = 0, sticky = "news")
+        
+        
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_columnconfigure(2, weight = 1)
+        
+    def main(self):
+        Screen.current = 0
+        Screen.switch_frame()      
+        
+class Games(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+        self.lbl_history = tk.Label(self, text ="Games Quiz", font = TITLE_FONT)
+        self.lbl_history.grid(row = 0, column = 0, sticky = "news")
+        self.btn_start = tk.Button(self, text = "Start!", font = BUTTON_FONT)
+        self.btn_start.grid(row = 1, column = 0, sticky = "news")
+        self.btn_back = tk.Button(self, text = "Back", font = BUTTON_FONT, command = self.main)
+        self.btn_back.grid(row = 2, column = 0, sticky = "news")
+        
+        
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_columnconfigure(2, weight = 1)
+        
+    def main(self):
+        Screen.current = 0
+        Screen.switch_frame()
+        
+class Random(Screen):
+    def __init__(self):
+        Screen.__init__(self)
+        self.lbl_history = tk.Label(self, text ="Random Question Quiz", font = TITLE_FONT)
+        self.lbl_history.grid(row = 0, column = 0, sticky = "news")
+        self.btn_start = tk.Button(self, text = "Start!", font = BUTTON_FONT)
+        self.btn_start.grid(row = 1, column = 0, sticky = "news")
+        self.btn_back = tk.Button(self, text = "Back", font = BUTTON_FONT, command = self.main)
+        self.btn_back.grid(row = 2, column = 0, sticky = "news")
+        
+        
+        self.grid_columnconfigure(0, weight = 1)
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_columnconfigure(2, weight = 1)
+        
+    def main(self):
+        Screen.current = 0
+        Screen.switch_frame()
+        
+class History_Questions(Screen):
     def __init__(self):
         Screen.__init__(self)
         
@@ -103,8 +218,12 @@ if __name__ == "__main__":
     root.geometry("500x500")
     root.grid_columnconfigure(0, weight = 1)
     root.grid_rowconfigure(0, weight = 1)    
-    screens = [MainMenu(), History()]
+    screens = [MainMenu(), History(), Geography(), Music(), Games(), Random()]
     screens[0].grid(row = 0, column = 0, sticky = "news")
     screens[1].grid(row = 0, column = 0, sticky = "news")
+    screens[2].grid(row = 0, column = 0, sticky = "news")
+    screens[3].grid(row = 0, column = 0, sticky = "news")
+    screens[4].grid(row = 0, column = 0, sticky = "news")
+    screens[5].grid(row = 0, column = 0, sticky = "news")    
     screens[0].tkraise()
     root.mainloop()
