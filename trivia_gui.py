30f828bd27653da6cd95665fab079534c38c7a1c
Austin Andrews <aandrews@debian07.CIS.COM>
2020-03-12 12:05:22 -0500

    YY

--- trivia_gui.py
+++ trivia_gui.py
@@ -31,6 +31,7 @@ class Screen(tk.Frame):
 class MainMenu(Screen):
     def __init__(self):
         Screen.__init__(self)
+        Screen.__init__(self)
         self.lbl_title = tk.Label(self, text = "Trivia Menu", font = TITLE_FONT)
         self.lbl_title.grid(row = 0, column = 0)
         self.btn_add = tk.Button(self, text = "History", font = BUTTON_FONT, command = self.history)
@@ -43,7 +44,7 @@ class MainMenu(Screen):
         self.btn_remove.grid(row = 4, column = 0)
         self.btn_save = tk.Button(self, text = "Random", font = BUTTON_FONT, command = self.random)
         self.btn_save.grid(row = 5, column = 0)
-        
+
         self.grid_columnconfigure(0, weight = 1)
         self.grid_columnconfigure(1, weight = 1)  
         self.grid_columnconfigure(2, weight = 1)
@@ -66,7 +67,288 @@ class MainMenu(Screen):
         
     def random(self):
         Screen.current = 5
-        Screen.switch_frame()      
+        Screen.switch_frame()             
+        
+class History_Q1(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+            
+        
+class History_Q2(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+        
+class History_Q3(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+class History_Q4(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+        
+        
+class History_Q5(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+        
+        
+class History_Q6(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+        
+        
+class History_Q7(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+        
+        
+class History_Q8(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+        
+        
+class History_Q9(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+        
+        
+class History_Q10(Screen):
+    def __init__(self):
+        Screen.__init__(self)    
+        
+        question = "What is a foot?"
+        self.lbl_title = tk.Label(self, text = question, font = TITLE_FONT)
+        self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
+        
+        
+        #RadioButtons
+        self.rad_a = tk.Radiobutton(self, text = "Hi" , font = BUTTON_FONT, value=1)
+        self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
+        self.rad_b = tk.Radiobutton(self, text= "Hi" , font = BUTTON_FONT, value=2)
+        self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
+        self.rad_c = tk.Radiobutton(self, text="Hi" , font = BUTTON_FONT, value=3)
+        self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
+        self.rad_d = tk.Radiobutton(self, text ="Hi", font = BUTTON_FONT, value=4)
+        self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
+       
+        #Buttons
+        self.btn_next = tk.Button(self,text = "Next", font = BUTTON_FONT)
+        self.btn_next.grid(row = 5, column = 0)        
+
+        self.grid_columnconfigure(1, weight = 1)  
+        self.grid_rowconfigure(0, weight = 1)     
+        self.grid_rowconfigure(1, weight = 1)
+      
+               
+
+        
+
         
 class History(Screen):
     def __init__(self):
@@ -162,49 +444,6 @@ class Random(Screen):
     def main(self):
         Screen.current = 0
         Screen.switch_frame()
-        
-class History_Questions(Screen):
-    def __init__(self):
-        Screen.__init__(self)
-        
-        history_ans ={1 :["Banana", "Hot", "Random", "Hand"],
-                      2 :["Red", "Cold", "Apple", "Ear"]}
-
-            
-        
-        def set_up(self,answers):
-            
-            dog = "What is a foot?"
-            self.lbl_title = tk.Label(self, text = dog, font = TITLE_FONT)
-            self.lbl_title.grid(row = 0, column = 0, sticky = "news")   
-            
-            
-            #RadioButtons
-            self.rad_a = tk.Radiobutton(self, text = history_ans[answers][0], font = BUTTON_FONT, value=1)
-            self.rad_a.grid(row = 1, column = 0, sticky = "nw") 
-            self.rad_b = tk.Radiobutton(self, text = history_ans[answers][1], font = BUTTON_FONT, value=2)
-            self.rad_b.grid(row = 2, column = 0, sticky = "nw") 
-            self.rad_c = tk.Radiobutton(self, text = history_ans[answers][2], font = BUTTON_FONT, value=3)
-            self.rad_c.grid(row = 3, column = 0, sticky = "nw") 
-            self.rad_d = tk.Radiobutton(self, text = history_ans[answers][3], font = BUTTON_FONT, value=4)
-            self.rad_d.grid(row = 4, column = 0, sticky = "nw") 
-            
-            #Buttons
-            self.btn_save = tk.Button(self,text = "Next", font = BUTTON_FONT)
-            self.btn_save.grid(row = 5, column = 0)        
-            
-    
-    
-            self.grid_columnconfigure(1, weight = 1)  
-            self.grid_rowconfigure(0, weight = 1)     
-            self.grid_rowconfigure(1, weight = 1)
-            self.grid_rowconfigure(2, weight = 1)
-            self.grid_rowconfigure(3, weight = 1)
-            self.grid_rowconfigure(4, weight = 1)        
-         
-            
-        answers = 1
-        set_up(self, answers)
     
     
 #Main
@@ -218,12 +457,13 @@ if __name__ == "__main__":
     root.geometry("500x500")
     root.grid_columnconfigure(0, weight = 1)
     root.grid_rowconfigure(0, weight = 1)    
-    screens = [MainMenu(), History(), Geography(), Music(), Games(), Random()]
+    screens = [MainMenu(), History(), Geography(), Music(), Games(), Random(), History_Q1()]
     screens[0].grid(row = 0, column = 0, sticky = "news")
     screens[1].grid(row = 0, column = 0, sticky = "news")
     screens[2].grid(row = 0, column = 0, sticky = "news")
     screens[3].grid(row = 0, column = 0, sticky = "news")
     screens[4].grid(row = 0, column = 0, sticky = "news")
-    screens[5].grid(row = 0, column = 0, sticky = "news")    
+    screens[5].grid(row = 0, column = 0, sticky = "news") 
+    screens[6].grid(row = 0, column = 0, sticky = "news")  
     screens[0].tkraise()
     root.mainloop()
